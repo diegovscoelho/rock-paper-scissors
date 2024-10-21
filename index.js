7feee1c80436f8e1e81898f25e9a3a2f703c5edd
Diego <dcoelhocarneiro@gmail.com>
2024-10-21 10:32:55 -0300

    User Interface

--- index.js
+++ index.js
@@ -9,52 +9,64 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Escolha uma opção entre pedra, papel e tesoura:");
-    return humanChoice;
-}
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const show = document.querySelector("#show");
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+    
+    rock.addEventListener("click", () => {
+        humanSelection = "pedra";
+        computerSelection = getComputerChoice();
+        playRound();
+    });
+
+    paper.addEventListener("click", () => {
+        humanSelection = "papel";
+        computerSelection = getComputerChoice();
+        playRound();
+    });
+
+    scissors.addEventListener("click", () => {
+        humanSelection = "tesoura";
+        computerSelection = getComputerChoice();
+        playRound();
+    });
 
     function playRound() {
-        
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        if(humanSelection.toLowerCase() === "pedra" && computerSelection.toLowerCase() === "tesoura") {
-            console.log("Parabéns! Pedra ganha de tesoura.");
+        if (humanSelection === computerSelection) {
+            console.log(`Empate! Ambos escolheram ${humanSelection}.`);
+        } else if (humanSelection === "pedra" && computerSelection === "tesoura" ||
+                   humanSelection === "papel" && computerSelection === "pedra" ||
+                   humanSelection === "tesoura" && computerSelection === "papel") {
+            console.log(`Parabéns! ${humanSelection} ganha de ${computerSelection}.`);
             humanScore += 1;
-        } else if(humanSelection.toLowerCase() === "papel" && computerSelection.toLowerCase() === "pedra") {
-            console.log("Parabéns! Papel ganha de pedra.");
-            humanScore += 1;
-        } else if(humanSelection.toLowerCase() === "tesoura" && computerSelection.toLowerCase() === "papel") {
-            console.log("Parabéns! Tesoura ganha de papel.");
-            humanScore += 1;
-        } else if(humanSelection.toLowerCase() === "pedra" && computerSelection.toLowerCase() === "papel") {
-            console.log("Você perdeu! Papel ganha de pedra.");
-            computerScore += 1;
-        } else if(humanSelection.toLowerCase() === "papel" && computerSelection.toLowerCase() === "tesoura") {
-            console.log("Você perdeu! Tesoura ganha de papel.");
-            computerScore += 1;
         } else {
-            console.log("Você perdeu! Pedra ganha de tesoura.");
+            console.log(`Você perdeu! ${computerSelection} ganha de ${humanSelection}.`);
             computerScore += 1;
+        }
+
+        if((humanScore || computerScore) === 5) {
+            if(humanScore === 5) {
+                console.log("Parabéns, você venceu!");
+            } else {
+                console.log("Que pena! Você perdeu");
+            }
         }
     }
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-
-    if(humanScore > computerScore) {
-        return console.log("Parabéns, você ganhou no geral!");
-    } else {
-        return console.log("Você perdeu no geral!");
-    }
+    show.addEventListener("click", () => {
+        if(humanScore > computerScore) {
+            console.log(`Parabéns, você está ganhando! ${humanScore} ${computerScore}`);
+        } else if(computerScore > humanScore) {
+            console.log(`Que pena! Você está perdendo. ${humanScore} a ${computerScore}`);
+        }
+    });    
 }
 
 playGame();
